20ee9c0a16699b64598bb0f7aa07652a03d142bf
shengyu <sheldo.cn@gmail.com>
2019-08-05 08:15:15 +0800

    redux ssr

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import Routes from '../Routes';
+// import
 
 const App = ()=>{
     return (
