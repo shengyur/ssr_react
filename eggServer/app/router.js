77be8f13efbc8986ef2822fd99c34bed75cf6c46
shengyur <sheldo.cn@gmaiil.com>
2019-08-09 18:40:14 +0800

    优化render

--- eggServer/app/router.js
+++ eggServer/app/router.js
@@ -5,6 +5,6 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  // router.get('/', controller.home.index);
   router.get('/list', controller.home.getList);
 };
--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { render } from './utils';
+import { getStore } from '../store';
+import { matchRoutes } from "react-router-config";
+import routes from '../Routes';
 
 const app = express();
 //使用express去访问静态文件时，到根目录的public中找
@@ -7,7 +10,19 @@ app.use(express.static('public'));
 
 //* 表示匹配所有路由路径
 app.get('*', function (req, res) {
-    render(req, res)
+    const store = getStore();
+    const branch = matchRoutes(routes, req.path)
+    let promises = [];
+    branch.forEach((item) => {
+        if (item.route.loadData) {
+            promises.push(item.route.loadData(store))
+        }
+    })
+
+    Promise.all(promises).then(() => {
+        res.send(render(store, routes, req))
+    })
+
 })
 
 const server = app.listen(3000)
